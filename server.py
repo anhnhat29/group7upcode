1598642fd8fd332e778345591eaf262e64497a0a
Anhnhat <nxtlikeyou@gmail.com>
2025-09-17 14:23:04 +0700

    websocket

--- server.py
+++ server.py
@@ -7,15 +7,17 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.htmml')  # Đảm bảo file này tồn tại
+    return render_template('index.html')  # Đảm bảo file này tồn tại
 
 @socketio.on('connect')
 def handle_connect():
     # Đọc dữ liệu từ file JSON
-    with open('data.json', 'r', encoding='utf-8') as f:
+    with open('Json_data.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
     # Gửi dữ liệu lên client qua WebSocket
     socketio.emit('json_data', data)
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
+    
+    
--- testwebsk.py
+++ testwebsk.py
@@ -7,14 +7,16 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.htmml')  # Đảm bảo file này tồn tại
+    return render_template('index2.html')
 
 @socketio.on('connect')
 def handle_connect():
-    with open('Json_data.json', 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    emit('json_data', data)
+    try:
+        with open('json_data.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        emit('json_data', data)
+    except Exception as e:
+        emit('json_data', {"error": str(e)})
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
-    
